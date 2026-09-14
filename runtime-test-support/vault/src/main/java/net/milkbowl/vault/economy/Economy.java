20556359b73a8c5fe3dc78a80aa76688d860d6e5
Pasta0719 <95007096+Pasta0719@users.noreply.github.com>
2026-09-14 21:55:02 +0900

    Phase08: extend test Vault contract with deposit

--- runtime-test-support/vault/src/main/java/net/milkbowl/vault/economy/Economy.java
+++ runtime-test-support/vault/src/main/java/net/milkbowl/vault/economy/Economy.java
@@ -6,4 +6,5 @@ import org.bukkit.OfflinePlayer;
 public interface Economy {
     double getBalance(OfflinePlayer player);
     EconomyResponse withdrawPlayer(OfflinePlayer player, double amount);
+    EconomyResponse depositPlayer(OfflinePlayer player, double amount);
 }
