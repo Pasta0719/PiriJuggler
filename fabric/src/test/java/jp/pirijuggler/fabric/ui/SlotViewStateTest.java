7ac7fe1037721b66ee6a02d340ff82515c0ce71b
Pasta0719 <95007096+Pasta0719@users.noreply.github.com>
2026-09-14 13:21:14 +0900

    Update Fabric reel expectations for faster normal speed

--- fabric/src/test/java/jp/pirijuggler/fabric/ui/SlotViewStateTest.java
+++ fabric/src/test/java/jp/pirijuggler/fabric/ui/SlotViewStateTest.java
@@ -22,16 +22,16 @@ class SlotViewStateTest {
         var time=new AtomicLong();var view=open(time);var b=start().payload();b.getAsJsonObject("startPhase").addProperty("left",20.1);view.receive(Envelope.current(PacketType.SPIN_START,b));
         view.receive(packet(PacketType.REEL_STOP,"{\"spinId\":\""+SPIN+"\",\"reel\":\"LEFT\",\"stopIndex\":0,\"durationMs\":80}"));
         time.set(80_000_000);double after80=view.phase(0);assertTrue(after80<20.1&&after80>18.5,"must continue downward without a visual sprint");
-        time.set(1_117_000_000L);assertEquals(0,view.phase(0),1e-9);
+        time.set(958_000_000L);assertEquals(0,view.phase(0),1e-9);
     }
     static final String ID="00000000-0000-0000-0000-000000000001",SPIN="00000000-0000-0000-0000-000000000002";
     static Envelope packet(PacketType type,String json){return Envelope.current(type,JsonParser.parseString(json).getAsJsonObject());}
     static SlotViewState open(AtomicLong clock){var view=new SlotViewState(clock::get);view.receive(packet(PacketType.OPEN_MACHINE,"{\"sessionId\":\""+ID+"\",\"machineId\":1}"));return view;}
     static Envelope start(){return packet(PacketType.SPIN_START,"{\"sessionId\":\""+ID+"\",\"machineId\":1,\"spinId\":\""+SPIN+"\",\"animation\":\"NORMAL\",\"startPhase\":{\"left\":8,\"center\":3,\"right\":12}}");}
     @Test void motionBoundariesAreContinuousAndRespectReceptionTime(){
-        assertEquals(0,SlotViewState.distance("NORMAL",.150));assertEquals(-3.15,SlotViewState.distance("NORMAL",.5),1e-9);assertEquals(-12.15,SlotViewState.distance("NORMAL",1),1e-9);
-        assertEquals(6,SlotViewState.distance("REVERSE_500MS",.5),1e-9);assertEquals(3.3,SlotViewState.distance("REVERSE_500MS",.8),1e-9);assertEquals(-18,SlotViewState.distance("RESUME_NORMAL",1));
-        var time=new AtomicLong(9_000_000_000L);var view=open(time);view.receive(start());assertEquals(8,view.phase(0));time.addAndGet(150_000_000);assertEquals(8,view.phase(0));time.addAndGet(350_000_000);assertEquals(4.85,view.phase(0),1e-9);
+        assertEquals(0,SlotViewState.distance("NORMAL",.150));assertEquals(-3.675,SlotViewState.distance("NORMAL",.5),1e-9);assertEquals(-14.175,SlotViewState.distance("NORMAL",1),1e-9);
+        assertEquals(6,SlotViewState.distance("REVERSE_500MS",.5),1e-9);assertEquals(2.85,SlotViewState.distance("REVERSE_500MS",.8),1e-9);assertEquals(-21,SlotViewState.distance("RESUME_NORMAL",1));
+        var time=new AtomicLong(9_000_000_000L);var view=open(time);view.receive(start());assertEquals(8,view.phase(0));time.addAndGet(150_000_000);assertEquals(8,view.phase(0));time.addAndGet(350_000_000);assertEquals(4.325,view.phase(0),1e-9);
     }
     @Test void ServerStopIndexWinsIncludingSixSlipAndWrongSpinIsIgnored(){
         var time=new AtomicLong();var view=open(time);view.receive(start());
@@ -46,7 +46,7 @@ class SlotViewStateTest {
     @Test void resumedDisplayKeepsAlreadyStoppedReelsFixed(){
         var time=new AtomicLong();var view=open(time);
         view.receive(packet(PacketType.PUBLIC_STATE,"{\"sessionId\":\""+ID+"\",\"machineId\":1,\"gameState\":\"NORMAL_SPINNING\",\"stoppedMask\":1,\"lampOn\":false,\"displayStops\":{\"left\":14,\"center\":3,\"right\":12}}"));
-        var b=start().payload();b.addProperty("animation","RESUME_NORMAL");view.receive(Envelope.current(PacketType.SPIN_START,b));time.set(1_000_000_000);assertEquals(14,view.phase(0));assertEquals(6,view.phase(1),1e-9);assertEquals(15,view.phase(2),1e-9);
+        var b=start().payload();b.addProperty("animation","RESUME_NORMAL");view.receive(Envelope.current(PacketType.SPIN_START,b));time.set(1_000_000_000);assertEquals(14,view.phase(0));assertEquals(3,view.phase(1),1e-9);assertEquals(12,view.phase(2),1e-9);
     }
     @Test void clientStopDelayUsesEachServerMotionProfile(){
         for(var profile:jp.pirijuggler.common.reel.ReelMotion.Profile.values()){
