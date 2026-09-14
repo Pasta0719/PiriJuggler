47b3de4df705c88deff4541cf9597cf7aa1d98a0
Pasta0719 <95007096+Pasta0719@users.noreply.github.com>
2026-09-14 19:24:18 +0900

    Phase07: add minimal test Vault economy contract

---
+++ runtime-test-support/vault/src/main/java/net/milkbowl/vault/economy/Economy.java
@@ -0,0 +1,9 @@
+package net.milkbowl.vault.economy;
+
+import org.bukkit.OfflinePlayer;
+
+/** Minimal runtime-acceptance-only Vault Economy contract used by PiriJuggler reflection. */
+public interface Economy {
+    double getBalance(OfflinePlayer player);
+    EconomyResponse withdrawPlayer(OfflinePlayer player, double amount);
+}
