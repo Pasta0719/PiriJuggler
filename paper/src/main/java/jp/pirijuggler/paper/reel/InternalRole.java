73ac2911a124c9433fa807fe70919ea2af37ffbc
Pasta0719 <95007096+Pasta0719@users.noreply.github.com>
2026-09-15 22:59:38 +0900

    Expose direct bonus entry display role

--- paper/src/main/java/jp/pirijuggler/paper/reel/InternalRole.java
+++ paper/src/main/java/jp/pirijuggler/paper/reel/InternalRole.java
@@ -6,6 +6,13 @@ public enum InternalRole {
         if(premiumB){if(this!=CHERRY_BIG)throw new IllegalArgumentException("Premium B requires CHERRY_BIG");return DisplayRole.PREMIUM_B;}
         return switch(this){case BIG,REG->DisplayRole.BONUS;case CHERRY_BIG,CHERRY_REG->DisplayRole.BONUS_CHERRY;case PIERO_BIG,PIERO_REG->DisplayRole.PIERO_BONUS;case MISS->DisplayRole.MISS;case CHERRY->DisplayRole.CHERRY;case PIERO->DisplayRole.PIERO;case GRAPE->DisplayRole.GRAPE;case REPLAY->DisplayRole.REPLAY;case BELL->DisplayRole.BELL;};
     }
+    public DisplayRole directEntryDisplay(){
+        return switch(this){
+            case BIG,CHERRY_BIG,PIERO_BIG->DisplayRole.BIG_ENTRY;
+            case REG,CHERRY_REG,PIERO_REG->DisplayRole.REG_ENTRY;
+            default->null;
+        };
+    }
     public boolean premiumFEligible(){return this==BIG||this==CHERRY_BIG||this==PIERO_BIG;}
     /** Right-first top=GRAPE, middle=SEVEN, bottom=BAR is a grape-or-bonus tell. Piero overlaps are intentionally exempt. */
     public boolean allowsRightFirstGrapeSevenBar(){
