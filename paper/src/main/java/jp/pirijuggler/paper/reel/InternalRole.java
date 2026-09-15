07bd900fab83c5130409c98cf9086325b8ae2966
Pasta0719 <95007096+Pasta0719@users.noreply.github.com>
2026-09-15 22:16:29 +0900

    Classify right-first bonus tell eligibility

--- paper/src/main/java/jp/pirijuggler/paper/reel/InternalRole.java
+++ paper/src/main/java/jp/pirijuggler/paper/reel/InternalRole.java
@@ -7,4 +7,8 @@ public enum InternalRole {
         return switch(this){case BIG,REG->DisplayRole.BONUS;case CHERRY_BIG,CHERRY_REG->DisplayRole.BONUS_CHERRY;case MISS->DisplayRole.MISS;case CHERRY->DisplayRole.CHERRY;case PIERO,PIERO_BIG,PIERO_REG->DisplayRole.PIERO;case GRAPE->DisplayRole.GRAPE;case REPLAY->DisplayRole.REPLAY;case BELL->DisplayRole.BELL;};
     }
     public boolean premiumFEligible(){return this==BIG||this==CHERRY_BIG||this==PIERO_BIG;}
+    /** Right-first top=GRAPE, middle=SEVEN, bottom=BAR is a grape-or-bonus tell. Piero overlaps are intentionally exempt. */
+    public boolean allowsRightFirstGrapeSevenBar(){
+        return switch(this){case GRAPE,BIG,REG,CHERRY_BIG,CHERRY_REG,PIERO_BIG,PIERO_REG->true;default->false;};
+    }
 }
