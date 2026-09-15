7ab2264ce65e977a1eed1681f85aad478f8da3c4
Pasta0719 <95007096+Pasta0719@users.noreply.github.com>
2026-09-15 22:19:26 +0900

    Separate Piero overlap display role

--- paper/src/main/java/jp/pirijuggler/paper/reel/InternalRole.java
+++ paper/src/main/java/jp/pirijuggler/paper/reel/InternalRole.java
@@ -4,7 +4,7 @@ public enum InternalRole {
     MISS, REPLAY, GRAPE, CHERRY, BELL, PIERO, BIG, REG, CHERRY_BIG, CHERRY_REG, PIERO_BIG, PIERO_REG;
     public DisplayRole display(boolean premiumB){
         if(premiumB){if(this!=CHERRY_BIG)throw new IllegalArgumentException("Premium B requires CHERRY_BIG");return DisplayRole.PREMIUM_B;}
-        return switch(this){case BIG,REG->DisplayRole.BONUS;case CHERRY_BIG,CHERRY_REG->DisplayRole.BONUS_CHERRY;case MISS->DisplayRole.MISS;case CHERRY->DisplayRole.CHERRY;case PIERO,PIERO_BIG,PIERO_REG->DisplayRole.PIERO;case GRAPE->DisplayRole.GRAPE;case REPLAY->DisplayRole.REPLAY;case BELL->DisplayRole.BELL;};
+        return switch(this){case BIG,REG->DisplayRole.BONUS;case CHERRY_BIG,CHERRY_REG->DisplayRole.BONUS_CHERRY;case PIERO_BIG,PIERO_REG->DisplayRole.PIERO_BONUS;case MISS->DisplayRole.MISS;case CHERRY->DisplayRole.CHERRY;case PIERO->DisplayRole.PIERO;case GRAPE->DisplayRole.GRAPE;case REPLAY->DisplayRole.REPLAY;case BELL->DisplayRole.BELL;};
     }
     public boolean premiumFEligible(){return this==BIG||this==CHERRY_BIG||this==PIERO_BIG;}
     /** Right-first top=GRAPE, middle=SEVEN, bottom=BAR is a grape-or-bonus tell. Piero overlaps are intentionally exempt. */
