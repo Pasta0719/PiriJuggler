e05c39e2d9b9e76ef85f3a6edd60c262d80536f5
Pasta0719 <95007096+Pasta0719@users.noreply.github.com>
2026-09-16 23:24:19 +0900

    Test two-second minimum game-start interval

--- fabric/src/test/java/jp/pirijuggler/fabric/ui/ReelTimingGateTest.java
+++ fabric/src/test/java/jp/pirijuggler/fabric/ui/ReelTimingGateTest.java
@@ -24,18 +24,30 @@ class ReelTimingGateTest {
         time.set(700_000_000L);assertTrue(view.canSend(PacketType.STOP_LEFT));
     }
 
-    @Test void nextGameLeverWaitIsExactlySeventeenHundredMillisecondsAfterThirdStop(){
+    @Test void nextGameWaitIsOnlyTheRemainderOfTwoSecondsFromPreviousSpinStart(){
         var time=new AtomicLong();
         var view=SlotViewStateTest.open(time);
         view.receive(SlotViewStateTest.start());
+        time.set(1_200_000_000L);
+        settle(view);
+        assertEquals(800_000_000L,view.nextGameRemainingNanos());
+        assertTrue(view.shouldQueueLever());assertFalse(view.queuedLeverReady());
+        time.set(1_999_999_999L);assertTrue(view.shouldQueueLever());assertFalse(view.queuedLeverReady());
+        time.set(2_000_000_000L);assertFalse(view.shouldQueueLever());assertTrue(view.queuedLeverReady());
+
+        var lateTime=new AtomicLong();
+        var late=SlotViewStateTest.open(lateTime);
+        late.receive(SlotViewStateTest.start());
+        lateTime.set(2_100_000_000L);
+        settle(late);
+        assertEquals(0,late.nextGameRemainingNanos());
+        assertFalse(late.shouldQueueLever());assertTrue(late.queuedLeverReady());
+    }
+
+    private static void settle(SlotViewState view){
         view.receive(SlotViewStateTest.packet(PacketType.REEL_STOP,"{\"spinId\":\""+SlotViewStateTest.SPIN+"\",\"reel\":\"LEFT\",\"stopIndex\":8,\"durationMs\":0}"));
         view.receive(SlotViewStateTest.packet(PacketType.REEL_STOP,"{\"spinId\":\""+SlotViewStateTest.SPIN+"\",\"reel\":\"CENTER\",\"stopIndex\":3,\"durationMs\":0}"));
         view.receive(SlotViewStateTest.packet(PacketType.REEL_STOP,"{\"spinId\":\""+SlotViewStateTest.SPIN+"\",\"reel\":\"RIGHT\",\"stopIndex\":12,\"durationMs\":0}"));
         view.receive(SlotViewStateTest.packet(PacketType.PUBLIC_STATE,"{\"sessionId\":\""+SlotViewStateTest.ID+"\",\"machineId\":1,\"gameState\":\"NORMAL_BETTED\",\"lampOn\":false,\"displayStops\":{\"left\":8,\"center\":3,\"right\":12}}"));
-
-        assertEquals(1_700_000_000L,view.nextGameRemainingNanos());
-        assertTrue(view.shouldQueueLever());assertFalse(view.queuedLeverReady());
-        time.set(1_699_999_999L);assertTrue(view.shouldQueueLever());assertFalse(view.queuedLeverReady());
-        time.set(1_700_000_000L);assertFalse(view.shouldQueueLever());assertTrue(view.queuedLeverReady());
     }
 }
