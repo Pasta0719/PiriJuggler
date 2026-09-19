3ba901c97daddcc946a8c9727b0d3ec833e662e0
Pasta0719 <95007096+Pasta0719@users.noreply.github.com>
2026-09-19 19:41:54 +0900

    phase12: bump protocol for remote machine state

--- common/src/main/java/jp/pirijuggler/common/protocol/Protocol.java
+++ common/src/main/java/jp/pirijuggler/common/protocol/Protocol.java
@@ -3,7 +3,7 @@ package jp.pirijuggler.common.protocol;
 public final class Protocol {
     public static final String CHANNEL = "piri:main";
     public static final int MAGIC = 0x50495249;
-    public static final int VERSION = 1;
+    public static final int VERSION = 2;
     public static final String MOD_VERSION = "1.0.0";
     public static final String SERVER_VERSION = "1.0.0";
     public static final int MAX_PAYLOAD_BYTES = 32767;
